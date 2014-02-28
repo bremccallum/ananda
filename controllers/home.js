c0bf03d70c4718bae8776c0d4cd6a14de4a71975
Matt Enlow <matt@novaugust.net>
2014-02-28 07:53:58 -0700

    Getting classes by program IDs, added workshops to landing page

--- controllers/home.js
+++ controllers/home.js
@@ -26,15 +26,37 @@ module.exports = function (soap) {
             SchedulingWindow: true,
             XMLDetail: 'Bare'
         };
-        var tmrwStart = tmrw.startOf('day');
-        var tdayEnd = now.endOf('day');
+        var workshopArgs = {
+            Fields: [
+                {
+                    string: 'Classes.ClassDescription.Name'
+                },
+                {
+                    string: 'Classes.StartDateTime'
+                },
+                {
+                    string: 'Classes.Staff.Name'
+                }
+            ],
+            StartDateTime: now.format('YYYY-MM-DD[T]HH:mm:ss'),
+            EndDateTime: moment(tmrw).add('months', 2).format('YYYY-MM-DD[T]HH:mm:ss'),
+            SchedulingWindow: true,
+            ProgramIDs: [
+                {
+                    int: 27
+                }
+            ],
+            XMLDetail: 'Bare'
+        }
         //get classes
-        soap.q(soap.Classes, 'GetClasses', args)
-            .done(function (result) {
-                var classes = result[0].GetClassesResult.Classes.Class;
+        Q.all([soap.q(soap.Classes, 'GetClasses', args),
+               soap.q(soap.Classes, 'GetClasses', workshopArgs)])
+            .spread(function (classes, workshops) {
+                classes = classes[0].GetClassesResult.Classes.Class;
                 if (classes.length === undefined) {
                     classes = [classes];
                 }
+                var tmrwStart = tmrw.startOf('day');
                 var model = {
                     today: classes.filter(function (ele) {
                         var d = moment(ele.StartDateTime);
@@ -42,9 +64,10 @@ module.exports = function (soap) {
                     }),
                     tmrw: classes.filter(function (ele) {
                         return moment(ele.StartDateTime).isAfter(now.endOf('day'));
-                    })
+                    }),
+                    workshops: workshops[0].GetClassesResult.Classes.Class,
+                    pm: now.hours() >= 16
                 };
-                model.pm = now.hours() >= 16;
                 res.render('landing.html', Page("Ananda Yoga", model));
 
             });
@@ -86,6 +109,11 @@ module.exports = function (soap) {
     function classes(req, res) {
         var args = {
             XMLDetail: 'Bare',
+            ProgramIDs: [
+                {
+                    int: 22 //Dropins
+                }
+            ],
             Fields: [
                 {
                     string: 'ClassDescriptions.ImageURL'
@@ -95,19 +123,12 @@ module.exports = function (soap) {
                 },
                 {
                     string: 'ClassDescriptions.Description'
-                },
-                {
-                    string: 'ClassDescriptions.Program.Name' //Needed for filter
                 }
             ]
         };
         soap.q(soap.Classes, 'GetClassDescriptions', args)
             .done(function (classes) {
-                classes = classes[0].GetClassDescriptionsResult.ClassDescriptions.ClassDescription
-                    .filter(function (e) {
-                        var n = e.Program.Name;
-                        return n != 'Workshops' && n != "Special Events" && typeof e.Description === 'string';
-                    });
+                classes = classes[0].GetClassDescriptionsResult.ClassDescriptions.ClassDescription;
                 classes.map(function (o, i) {
                     o.Description = (typeof o.Description == 'object') ? '' : o.Description;
                 });
@@ -143,9 +164,9 @@ module.exports = function (soap) {
         soap.q(soap.Classes, 'GetClasses', args)
             .done(function (classes) {
                 var model = {
-                        classes: {},
-                        days : []
-                    };
+                    classes: {},
+                    days: []
+                };
                 for (var i = 0; i <= future.diff(now, 'days'); i++) {
                     var m = moment(now).add('days', i).format("dddd [the] Do");
                     model.days.push(m);
@@ -154,7 +175,7 @@ module.exports = function (soap) {
                 classes[0].GetClassesResult.Classes.Class.forEach(function (c) {
                     model.classes[moment(c.StartDateTime).format("dddd [the] Do")].push(c);
                 });
-                
+
                 res.render("schedule.html", Page("Schedule | Ananda Yoga", model));
             });
     }
