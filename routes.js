b3a1013b87add5d6eca6ee09b78f0b0bd6faccbc
Matt Enlow <matt@novaugust.net>
2014-03-01 18:15:50 -0700

    Misc code updates

--- routes.js
+++ routes.js
@@ -16,12 +16,9 @@ require("./soap")(function (cli) {
     app.get('/news/:slug', function(req, res){
         res.send("Under construction");
     });
+    
+    
     app.get('/admin', admin.dashboard);
-    /*
-======================================================================
-Posts
-======================================================================
-    */
     app.get('/admin/post', admin.newPost);
     app.get('/admin/post/:slug', admin.editPost);
     app.put('/admin/post', admin.updatePost);
