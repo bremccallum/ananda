7c2eacdf5e4df20be0322b3f9df10402b1b22d3e
Matt Enlow <matt@novaugust.net>
2014-04-28 16:48:13 -0600

    Added notes to fix up session handlig

--- ananda.js
+++ ananda.js
@@ -4,8 +4,14 @@ var express = require('express'),
 
 app.configure(function () {
     app.locals.prod = process.env.NODE_ENV == "production";
+    // ### Forms
     app.use(express.json());
     app.use(express.urlencoded());
+    
+    // ### Session
+    // @TODO
+    //   Okay, so maybe it's not the most secure cookie 
+    //   system and I have research to do.
     app.use(express.cookieParser('super secret string'));
     app.use('/admin', function (req, res, next) {
         if (req.signedCookies.loggedin === "true") {
@@ -16,11 +22,14 @@ app.configure(function () {
             res.redirect("/login");
         }
     });
+    
+    // ### Statics
     app.use('/styles', express.static(__dirname + '/client/public/styles'));
     app.use('/fonts', express.static(__dirname + '/client/public/fonts'));
     app.use('/img', express.static(__dirname + '/client/public/img'));
     app.use('/js', express.static(__dirname + '/client/public/js'));
     app.use('/uploads', express.static(__dirname + '/client/uploads'));
+    // ## Load the app!
     server.initialize(app);
 });
 
